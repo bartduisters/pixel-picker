7e619bfbb438259702ced8dc35299906ea7b65d6
Bart Duisters <bartduisters@bartduisters.com>
2020-08-08 22:46:01 +0200

    New JavaFX application - using Maven

---
+++ src/main/java/com/bartduisters/Controller.java
@@ -0,0 +1,4 @@
+package com.bartduisters;
+
+public class Controller {
+}
---
+++ src/main/java/com/bartduisters/Main.java
@@ -0,0 +1,7 @@
+package com.bartduisters;
+
+public class Main {
+    public static void main(String[] args) {
+        PixelPicker.main(args);
+    }
+}
